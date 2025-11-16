41908e92d75b7a0c681a29bff01dea67368eb8a6
Dragon56YT <89802059+Dragon56YT@users.noreply.github.com>
2025-11-16 15:26:52 +0100

    Update escape_terminal.py

--- escape_terminal.py
+++ escape_terminal.py
@@ -2,6 +2,10 @@
 Escape the Terminal - single-file Python roguelike for the terminal
 By: Dragon56YT
 Version: v0.8-beta
+
+Escape the Terminal © 2025 by Dragon56YT is licensed under Creative Commons Attribution-NonCommercial 4.0 International.
+To view a copy of this license, visit https://creativecommons.org/licenses/by-nc/4.0/
+
 """
 
 import curses
